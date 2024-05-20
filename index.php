01fc7fcfc94b6cfe77318a04a62796f389e74d65
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2024-05-20 14:28:47 +0200

    fix: use long domain after fix

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 // this is just a helper file for DDEV, redirect to our app when using `ddev launch`
-$redirect = 'https://app.'.$_SERVER['DDEV_SITENAME'].'.'.$_SERVER['DDEV_TLD'];
-header('Location: '.$redirect);
+$redirect = 'https://sveltekit-app.' . $_SERVER['DDEV_SITENAME'] . '.' . $_SERVER['DDEV_TLD'];
+header('Location: ' . $redirect);
